e5bc886e36ab15367133f47aa9ff319f7c1a0555
Agne Nava <sak.agne@gmail.com>
2022-10-02 21:20:24 +0300

    Main models

--- notes_mine/models.py
+++ notes_mine/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+from tinymce.models import HTMLField
+from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 # Create your models here.
+
+class Note(models.Model):
+  title = models.CharField(_('Title'), max_length=200)
+  photo = models.ImageField(default=None, upload_to="images",)
+  text = models.TextField(_('Text'), max_length=2000)
+  category = models.ManyToManyField('Category')
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    verbose_name = 'Zinute'
+    verbose_name_plural = 'Zinutes'
+
+  def display_category(self):
+    return ', '.join(category.name for category in self.category.all()[:3])
+
+  display_category.short_description = 'Kategorija'
+
+
+class Category(models.Model):
+  name = models.CharField(_('Name'), max_length=200)
+
+  def __str__(self):
+      return self.name
+
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  photo = models.ImageField(default="default.png", upload_to="profile_pics",)
+
+  def __str__(self):
+    return f"{self.user.username} profile"
+
+  class Meta:
+      verbose_name = 'Profilis'
+      verbose_name_plural = 'Profiliai'
+
+  def save(self, *args, **kwargs):
+      super().save(*args, **kwargs)
+      img = Image.open(self.nuotrauka.path)
+      if img.height > 300 or img.width > 300:
+          output_size = (300, 300)
+          img.thumbnail(output_size)
+          img.save(self.nuotrauka.path)
