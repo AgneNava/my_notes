2923af5904cc68d494def33fa2359101b3f958fe
Agne Nava <sak.agne@gmail.com>
2022-10-03 00:08:49 +0300

    User notes view and template

--- notes_mine/views.py
+++ notes_mine/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import Note, Profile, Category
 
 # Create your views here.
 
 def index(request):
   return render(request, 'notes_mine/index.html')
+
+
+class UserNotesListView(LoginRequiredMixin,generic.ListView):
+    model = Note
+    context_object_name = 'notes'
+    template_name ='notes_mine/user_notes.html'
+   
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user).order_by('title')
