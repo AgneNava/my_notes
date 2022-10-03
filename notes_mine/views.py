8f830ce5a1735e5306df320665a024cfab31cda5
Agne Nava <sak.agne@gmail.com>
2022-10-03 21:10:59 +0300

    Register overwrited

--- notes_mine/views.py
+++ notes_mine/views.py
@@ -15,7 +15,7 @@ from django.utils.translation import gettext as _
 from django.views.generic.edit import FormMixin
 
 from .models import Note, Profile, Category
-# from .forms import RegistrationForm
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -36,25 +36,17 @@ class UserNotesListView(LoginRequiredMixin,generic.ListView):
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        if password == password2:
-            if User.objects.filter(username=username).exists():
-                messages.error(request, f'Vartotojo vardas {username} užimtas!')
-                return redirect('notes_mine:register')
-            else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
-                    return redirect('notes_mine:register')
-                else:
-                    User.objects.create_user(username=username, email=email, password=password)
-                    messages.info(request, 'Registracija sekminga!')
-        else:
-            messages.error(request, 'Slaptažodžiai nesutampa!')
-            return redirect('notes_mine:register')
-    return redirect(request, 'notes_mine/register.html')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(username=form.cleaned_data['username'], 
+            email=form.cleaned_data['email'], 
+            password=form.cleaned_data['password'])   
+            messages.info(request, _('Registration successful!'))
+            return render(request, 'notes_mine/register.html')
+    else: 
+        form = RegistrationForm()
+
+    return render(request, 'notes_mine/register.html', {'form': form})
 
 # def register(request):
 #     if request.method == 'POST':
