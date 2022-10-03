c49865bd823f4bed6acdb374dbed0c1c75f1f7c1
Agne Nava <sak.agne@gmail.com>
2022-10-03 14:48:28 +0300

    Index page

--- notes_mine/views.py
+++ notes_mine/views.py
@@ -1,15 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from multiprocessing import context
+from django.urls import reverse, reverse_lazy
+from turtle import title
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import generic
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.contrib.auth.forms import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+from django.views.generic.edit import FormMixin
 
 from .models import Note, Profile, Category
+from .forms import RegistrationForm
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'notes_mine/index.html')
+    notes = Note.objects.all()
+    return render(request, 'notes_mine/index.html', context={'notes': notes})
+
+def test(request):
+  return render(request, 'notes_mine/test.html')
 
 
 class UserNotesListView(LoginRequiredMixin,generic.ListView):
@@ -19,3 +34,19 @@ class UserNotesListView(LoginRequiredMixin,generic.ListView):
    
     def get_queryset(self):
         return Note.objects.filter(user=self.request.user).order_by('title')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(username=form.cleaned_data['username'], 
+            email=form.cleaned_data['email'], 
+            password=form.cleaned_data['password'])   
+            messages.info(request, _('Registration successful!'))
+            return render(request, 'notes_mine/register.html')
+    else: 
+        form = RegistrationForm()
+
+    return render(request, 'notes_mine/register.html', {'form': form})
+
+
