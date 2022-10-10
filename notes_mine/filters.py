4b660185e789f365413ab1bb398cd69e87d0eefb
Agne Nava <sak.agne@gmail.com>
2022-10-10 21:28:38 +0300

    Final fixes

--- notes_mine/filters.py
+++ notes_mine/filters.py
@@ -7,3 +7,13 @@ class CatNotesFilter(django_filters.FilterSet):
     class Meta:
         model = Note
         fields = ['category']
+
+    # Neveikia:
+    # @property
+    # def qs(self):
+    #     parent = super().qs
+    #     user = getattr(self.request, 'user', None)
+    #     category = getattr(self.request, 'category', None)
+
+    #     return parent.filter(category=category) \
+    #         | parent.filter(user=user)
